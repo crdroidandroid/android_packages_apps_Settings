a21297e3fe43754250e1324cd3e78d4ea3fccbc1
cristianomatos <cristianobmatos@gmail.com>
2014-07-31 15:39:32 -0300

    [SQUASH] Revert CyanogenMod Heads Up and custom features for the same

--- src/com/android/settings/cyanogenmod/NotificationDrawer.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawer.java
@@ -39,16 +39,12 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private ListPreference mCollapseOnDismiss;
     private ListPreference mSmartPulldown;
     
-    private Preference mHeadsUp;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.notification_drawer);
         PreferenceScreen prefScreen = getPreferenceScreen();
-
-        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
 
         // Notification drawer
         int collapseBehaviour = Settings.System.getInt(getContentResolver(),
@@ -70,16 +66,6 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
             mSmartPulldown.setValue(String.valueOf(smartPulldown));
             updateSmartPulldownSummary(smartPulldown);
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        boolean headsUpEnabled = Settings.System.getInt(
-                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION, 0) == 1;
-        mHeadsUp.setSummary(headsUpEnabled
-                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
