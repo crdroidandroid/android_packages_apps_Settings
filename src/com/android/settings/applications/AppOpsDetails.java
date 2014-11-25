d15c8897089ef432f51ed25f2abd8d298360ec3c
Sam Mortimer <sam@mortimer.me.uk>
2014-11-25 14:18:22 -0800

    AppOps: show/hide app type and allow/deny counters [1/2]
    Change-Id: Id13f74b9787f93ec76e00ff959d9bb0369e102a6
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/applications/AppOpsDetails.java
+++ src/com/android/settings/applications/AppOpsDetails.java
@@ -114,6 +114,8 @@ public class AppOpsDetails extends InstrumentedPreferenceFragment {
                 String perm = AppOpsManager.opToPermission(firstOp.getOp());
                 ((TextView)view.findViewById(R.id.op_name)).setText(
                         entry.getSwitchText(mState));
+                ((TextView)view.findViewById(R.id.op_counts)).setText(
+                        entry.getCountsText(res));
                 ((TextView)view.findViewById(R.id.op_time)).setText(
                         entry.getTimeText(res, true));
                 Switch sw = (Switch)view.findViewById(R.id.switchWidget);
