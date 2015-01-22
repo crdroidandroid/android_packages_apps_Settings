eb6b40c1d0a4d51fe4a73a0fdff0cbc897c58da0
david <davidteri91@gmail.com>
2015-01-22 23:43:56 +0100

    Settings: Update crDroid
    Use correct tint color for banner
    Use the correct drawable for restore navbar dimensions

--- src/com/android/settings/crdroid/NavBarDimensions.java
+++ src/com/android/settings/crdroid/NavBarDimensions.java
@@ -114,7 +114,7 @@ public class NavBarDimensions extends SettingsPreferenceFragment implements
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
+                .setIcon(R.drawable.ic_settings_backup_restore) // use the restore icon
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
