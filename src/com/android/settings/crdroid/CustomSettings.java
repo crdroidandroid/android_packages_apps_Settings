9150be7d79802590e7937b435b201f3bb247b17d
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2015-02-02 07:43:33 -0200

    Settings: HeadsUp options [2/2]
    Includes:
    
    * Settings: forward port HeadsUp options [2/2]
    
    -Global on/off
    -Blacklist and Do not disturb
    
    (by Adnan Begovic)
    
    * HeadsUp settings: fix enabled state on clean install
    
    (by Altaf Patel)
    
    * My changes
    
    - Swype action selector (delete/hide)
    
    @cristianomatos: addapetd to crDroid sources
    
    Change-Id: Ia2ecc52034f04b11590abd62c5119f736985450a
    
    Conflicts:
    	res/values/cr_strings.xml

--- src/com/android/settings/crdroid/CustomSettings.java
+++ src/com/android/settings/crdroid/CustomSettings.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.content.res.Resources;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
@@ -28,15 +29,23 @@ public class CustomSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "CustomSettings";
 
+    private Preference mHeadsUp;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.crdroid_custom_settings);
+
+        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        boolean headsUpEnabled = Settings.System.getInt(
+                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION,1) != 0;
+        mHeadsUp.setSummary(headsUpEnabled
+                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
