5cca0fcc1b308e171b7f9d2c21079a8dd2997d45
david <davidteri91@gmail.com>
2015-02-12 00:41:03 +0100

    Settings: About crDroid
    Pick from commit: 8cdb88f3c9d228ae3acf0a0211ca6691bdcb0107  (Fix typo in About.java (string 55))

---
+++ src/com/android/settings/crdroid/About.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 crDroid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.crdroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mGoogleUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.crdroid_about);
+
+        mSiteUrl = findPreference("crdroid_website");
+        mSourceUrl = findPreference("crdroid_source");
+        mGoogleUrl = findPreference("crdroid_google_plus");
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://www.crdroid.org/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("https://github.com/crdroidandroid");
+        } else if (preference == mGoogleUrl) {
+            launchUrl("https://plus.google.com/u/0/communities/118297646046960923906");
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
