773d914facef2217a64d316d3c08755b6c272159
david <davidteri91@gmail.com>
2015-02-12 00:41:03 +0100

    Settings: About crDroid [SQUASH]
    Picked from commits:
                       5cca0fcc1b308e171b7f9d2c21079a8dd2997d45
                       5276c8bf769e7eb27b49cc1e73825ab293ca8358
                       a8e9da3587ff78a5ccde92ba1a58e977b8bbad71
                       a93be61996f0a0ffd4db93473c8271f1e8e0e1fc
    
    Conflicts:
    	res/values/cr_strings.xml
    	res/xml/device_info_settings.xml

---
+++ src/com/android/settings/crdroid/About.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2015 crDroid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.crdroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    private static final String KEY_CRDROID_SHARE = "share";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mGoogleUrl;
+    Preference mTranslationUrl;
+    Preference mEvolvedUrl;
+    Preference mNoRUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.crdroid_about);
+
+        mSiteUrl = findPreference("crdroid_website");
+        mSourceUrl = findPreference("crdroid_source");
+        mGoogleUrl = findPreference("crdroid_google_plus");
+        mTranslationUrl = findPreference("crdroid_translation");
+        mEvolvedUrl = findPreference("crdroid_theme_evolved");
+        mNoRUrl = findPreference("crdroid_theme_nor");
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://www.crdroid.org/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("https://github.com/crdroidandroid");
+        } else if (preference == mGoogleUrl) {
+            launchUrl("https://plus.google.com/u/0/communities/118297646046960923906");
+        } else if (preference == mTranslationUrl) {
+            launchUrl("https://oslt9io.oneskyapp.com/collaboration/");
+        } else if (preference == mEvolvedUrl) {
+            launchUrl("https://play.google.com/store/apps/details?id=com.cristianomatos.themecm12.evolved");
+        } else if (preference == mNoRUrl) {
+            launchUrl("https://play.google.com/store/apps/details?id=com.cristianomatos.themecm12.nor");
+        } else if (preference.getKey().equals(KEY_CRDROID_SHARE)) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, String.format(
+                getActivity().getString(R.string.share_message), Build.MODEL));
+        startActivity(Intent.createChooser(intent, getActivity().getString(R.string.share_chooser_title)));
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
