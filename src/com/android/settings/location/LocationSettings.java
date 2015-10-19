fda9ad444d4245423d862ae7721773d8e729378c
George G <kreach3r@users.noreply.github.com>
2015-10-19 04:19:38 +0300

    location: Enable 3dot menu
    * The code exists: https://github.com/android/platform_packages_apps_settings/commit/cb193d55e17fc7cfdb195eea9b6c4b4c7af66b4b
    * Stock Nexus image has it, so we have it, too. I don't how this got left out.
    
    Change-Id: If7eef74a4ba9b7e54a0f82560433ff404077fe66
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/location/LocationSettings.java
+++ src/com/android/settings/location/LocationSettings.java
@@ -145,6 +145,7 @@ public class LocationSettings extends LocationSettingsBase
         mSwitchBar = activity.getSwitchBar();
         mSwitch = mSwitchBar.getSwitch();
         mSwitchBar.show();
+        setHasOptionsMenu(true);
     }
 
     @Override
