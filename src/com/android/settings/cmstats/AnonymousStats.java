a91d616f5d6409f6a2f31ae2625bbfcd0a942753
cristianomatos <cristianobmatos@gmail.com>
2015-11-23 22:17:27 -0200

    Revert "Settings: add opt out stats event"
    This reverts commit 84c44757ac58d285aaaaf019c13c3afe9f664e80.

--- src/com/android/settings/cmstats/AnonymousStats.java
+++ src/com/android/settings/cmstats/AnonymousStats.java
@@ -20,26 +20,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import android.os.UserHandle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
+import android.util.ArraySet;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import cyanogenmod.providers.CMSettings;
+
+import java.util.Set;
 
 public class AnonymousStats extends SettingsPreferenceFragment {
-
     private static final String PREF_FILE_NAME = "CMStats";
     /* package */ static final String ANONYMOUS_OPT_IN = "pref_anonymous_opt_in";
     /* package */ static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
 
-    /* package */ static final String KEY_LAST_JOB_ID = "last_job_id";
+    /* package */ static final String KEY_JOB_QUEUE = "pref_job_queue";
     /* package */ static final int QUEUE_MAX_THRESHOLD = 1000;
-
-    public static final String KEY_STATS = "stats_collection";
-
-    SwitchPreference mStatsSwitch;
 
     public static SharedPreferences getPreferences(Context context) {
         return context.getSharedPreferences(PREF_FILE_NAME, 0);
@@ -49,51 +42,62 @@ public class AnonymousStats extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.anonymous_stats);
-        mStatsSwitch = (SwitchPreference) findPreference(KEY_STATS);
     }
 
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mStatsSwitch) {
-            boolean checked = mStatsSwitch.isChecked();
-            if (checked) {
-                // clear opt out flags
-                CMSettings.Secure.putIntForUser(getContentResolver(),
-                        CMSettings.Secure.STATS_COLLECTION_REPORTED, 0, UserHandle.USER_OWNER);
-            }
-            // will initiate opt out sequence if necessary
-            ReportingServiceManager.setAlarm(getActivity());
-            return true;
-        }
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    public static Set<String> getJobQueue(Context context) {
+        return getPreferences(context).getStringSet(KEY_JOB_QUEUE, new ArraySet<String>());
     }
 
-    public static void updateLastSynced(Context context) {
+    public static void clearJobQueue(Context context) {
         getPreferences(context)
                 .edit()
-                .putLong(ANONYMOUS_LAST_CHECKED,System.currentTimeMillis())
+                .remove(KEY_JOB_QUEUE)
                 .commit();
     }
 
-    private static int getLastJobId(Context context) {
-        return getPreferences(context).getInt(KEY_LAST_JOB_ID, 0);
-    }
+    public static void addJob(Context context, int jobId) {
+        Set<String> jobQueue = getJobQueue(context);
+        jobQueue.add(String.valueOf(jobId));
 
-    private static void setLastJobId(Context context, int id) {
         getPreferences(context)
                 .edit()
-                .putInt(KEY_LAST_JOB_ID, id)
+                .putStringSet(KEY_JOB_QUEUE, jobQueue)
                 .commit();
     }
 
+    public static void removeJob(Context context, int jobId) {
+        Set<String> jobQueue = getJobQueue(context);
+        jobQueue.remove(String.valueOf(jobId));
+        getPreferences(context)
+                .edit()
+                .remove(KEY_JOB_QUEUE)
+                .commit();
+
+        getPreferences(context)
+                .edit()
+                .putStringSet(KEY_JOB_QUEUE, jobQueue)
+                .commit();
+    }
+
+    /**
+     * @param context context to use to get prefs
+     * @return Returns the next unused int in the job queue, up until {@link #QUEUE_MAX_THRESHOLD}
+     * is reached, then it will return -1
+     */
     public static int getNextJobId(Context context) {
-        int lastId = getLastJobId(context);
-        if (lastId >= QUEUE_MAX_THRESHOLD) {
-            lastId = 1;
+        Set<String> currentQueue = getJobQueue(context);
+
+        if (currentQueue == null) {
+            return 1;
+        } else if (currentQueue.size() >= QUEUE_MAX_THRESHOLD) {
+            return -1;
         } else {
-            lastId += 1;
+            int i = 1;
+            while (currentQueue.contains(String.valueOf(i))) {
+                i++;
+            }
+            return i;
+
         }
-        setLastJobId(context, lastId);
-        return lastId;
     }
 }
--- src/com/android/settings/cmstats/StatsUploadJobService.java
+++ src/com/android/settings/cmstats/StatsUploadJobService.java
@@ -43,7 +43,7 @@ import java.util.Map;
 public class StatsUploadJobService extends JobService {
 
     private static final String TAG = StatsUploadJobService.class.getSimpleName();
-    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+    private static final boolean DEBUG = false;
 
     public static final String KEY_JOB_TYPE = "job_type";
     public static final int JOB_TYPE_CYANOGEN = 1;
@@ -56,7 +56,6 @@ public class StatsUploadJobService extends JobService {
     public static final String KEY_CARRIER = "carrier";
     public static final String KEY_CARRIER_ID = "carrierId";
     public static final String KEY_TIMESTAMP = "timeStamp";
-    public static final String KEY_OPT_OUT = "optOut";
 
     private final Map<JobParameters, StatsUploadTask> mCurrentJobs
             = Collections.synchronizedMap(new ArrayMap<JobParameters, StatsUploadTask>());
@@ -108,25 +107,16 @@ public class StatsUploadJobService extends JobService {
             String deviceCarrier = extras.getString(KEY_CARRIER);
             String deviceCarrierId = extras.getString(KEY_CARRIER_ID);
             long timeStamp = extras.getLong(KEY_TIMESTAMP);
-            boolean optOut = extras.getBoolean(KEY_OPT_OUT);
 
             boolean success = false;
-            int jobType = extras.getInt(KEY_JOB_TYPE, -1);
             if (!isCancelled()) {
+                int jobType = extras.getInt(KEY_JOB_TYPE, -1);
+
                 switch (jobType) {
                     case JOB_TYPE_CYANOGEN:
                         try {
-                            JSONObject json = new JSONObject();
-                            json.put("optOut", optOut);
-                            json.put("uniqueId", deviceId);
-                            json.put("deviceName", deviceName);
-                            json.put("version", deviceVersion);
-                            json.put("country", deviceCountry);
-                            json.put("carrier", deviceCarrier);
-                            json.put("carrierId", deviceCarrierId);
-                            json.put("timestamp", timeStamp);
-
-                            success = uploadToCyanogen(json);
+                            success = uploadToCyanogen(deviceId, deviceName, deviceVersion,
+                                    deviceCountry, deviceCarrier, deviceCarrierId, timeStamp);
                         } catch (IOException | JSONException e) {
                             Log.e(TAG, "Could not upload stats checkin to cyanogen server", e);
                             success = false;
@@ -136,7 +126,7 @@ public class StatsUploadJobService extends JobService {
                     case JOB_TYPE_CMORG:
                         try {
                             success = uploadToCM(deviceId, deviceName, deviceVersion, deviceCountry,
-                                    deviceCarrier, deviceCarrierId, optOut);
+                                    deviceCarrier, deviceCarrierId);
                         } catch (IOException e) {
                             Log.e(TAG, "Could not upload stats checkin to commnity server", e);
                             success = false;
@@ -148,6 +138,7 @@ public class StatsUploadJobService extends JobService {
             if (success) {
                 // we hit the server, succeed either which way.
                 mCurrentJobs.remove(mJobParams);
+                AnonymousStats.removeJob(StatsUploadJobService.this, mJobParams.getJobId());
             }
 
             if (DEBUG)
@@ -160,12 +151,10 @@ public class StatsUploadJobService extends JobService {
 
 
     private boolean uploadToCM(String deviceId, String deviceName, String deviceVersion,
-                               String deviceCountry, String deviceCarrier, String deviceCarrierId,
-                               boolean optOut)
+                               String deviceCountry, String deviceCarrier, String deviceCarrierId)
             throws IOException {
 
         final Uri uri = Uri.parse(getString(R.string.stats_cm_url)).buildUpon()
-                .appendQueryParameter("opt_out", optOut ? "1" : "0")
                 .appendQueryParameter("device_hash", deviceId)
                 .appendQueryParameter("device_name", deviceName)
                 .appendQueryParameter("device_version", deviceVersion)
@@ -193,13 +182,24 @@ public class StatsUploadJobService extends JobService {
 
     }
 
-    private boolean uploadToCyanogen(JSONObject json)
+    private boolean uploadToCyanogen(String deviceId, String deviceName, String deviceVersion,
+                                     String deviceCountry, String carrier, String carrierId,
+                                     long timeStamp)
             throws IOException, JSONException {
         String authToken = getAuthToken();
 
         if (authToken.isEmpty()) {
             Log.w(TAG, "no auth token!");
         }
+
+        JSONObject json = new JSONObject();
+        json.put("uniqueId", deviceId);
+        json.put("deviceName", deviceName);
+        json.put("version", deviceVersion);
+        json.put("country", deviceCountry);
+        json.put("carrier", carrier);
+        json.put("carrierId", carrierId);
+        json.put("timestamp", timeStamp);
 
         URL url = new URL(getString(R.string.stats_cyanogen_url));
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -223,13 +223,9 @@ public class StatsUploadJobService extends JobService {
 
             final int responseCode = urlConnection.getResponseCode();
             final boolean success = responseCode == HttpURLConnection.HTTP_OK;
-
-            final String response = getResponse(urlConnection, !success);
-            if (DEBUG)
-                Log.d(TAG, "server responseCode: " + responseCode +", response=" + response);
-
             if (!success) {
-                Log.w(TAG, "failed sending, server returned: " + response);
+                Log.w(TAG, "failed sending, server returned: " + getResponse(urlConnection,
+                        !success));
             }
             return success;
         } finally {
