4e6a5adeab23745fbcfdd5d83749cd1609a44094
George G <kreach3r@users.noreply.github.com>
2016-07-02 22:01:03 +0300

    InstalledApp: show link to Google Play
    Shows a Google Play store icon in the App Info actionbar of apps
    Excludes aosp built ones, but still works for apps generally found in /system, like SuperSU
    The openPlayStore() code was created by @nicholaschum for Substratum, thank you!
    BenzoEdit: fixed it up a tad to work with 7.1.1
    
    Change-Id: Ic55e572405d97b1f8ae3f8e3b5aceaa38b6a991e
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -39,6 +39,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.icu.text.ListFormatter;
 import android.net.INetworkStatsService;
 import android.net.INetworkStatsSession;
@@ -131,6 +132,7 @@ public class InstalledAppDetails extends AppInfoBase
     // Menu identifiers
     public static final int UNINSTALL_ALL_USERS_MENU = 1;
     public static final int UNINSTALL_UPDATES = 2;
+    public static final int PLAY_STORE = 3;
 
     // Result code identifiers
     public static final int REQUEST_UNINSTALL = 0;
@@ -511,6 +513,9 @@ public class InstalledAppDetails extends AppInfoBase
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
+                .setIcon(R.drawable.ic_menu_play_store)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(0, UNINSTALL_UPDATES, 0, R.string.app_factory_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(0, UNINSTALL_ALL_USERS_MENU, 1, R.string.uninstall_all_users_text)
@@ -530,6 +535,10 @@ public class InstalledAppDetails extends AppInfoBase
             RestrictedLockUtils.setMenuItemAsDisabledByAdmin(getActivity(),
                     uninstallUpdatesItem, mAppsControlDisallowedAdmin);
         }
+        // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
+        // and grab the Google Play Store itself (com.android.vending) in the process
+        menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
+                && !isAospOrStore(mAppEntry.info.packageName));
     }
 
     @Override
@@ -540,6 +549,9 @@ public class InstalledAppDetails extends AppInfoBase
                 return true;
             case UNINSTALL_UPDATES:
                 uninstallPkg(mAppEntry.info.packageName, false, false);
+                return true;
+            case PLAY_STORE:
+                openPlayStore(mAppEntry.info.packageName);
                 return true;
         }
         return false;
@@ -862,6 +874,18 @@ public class InstalledAppDetails extends AppInfoBase
             return mInstantAppButtonsController.createDialog(id);
         }
         return null;
+    }
+
+    private void openPlayStore(String packageName) {
+        // Launch an intent to the play store entry
+        String playURL = "https://play.google.com/store/apps/details?id=" + packageName;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(playURL));
+        startActivity(i);
+    }
+
+    private boolean isAospOrStore(String packageName) {
+        return packageName.contains("com.android");
     }
 
     private void uninstallPkg(String packageName, boolean allUsers, boolean andDisable) {
