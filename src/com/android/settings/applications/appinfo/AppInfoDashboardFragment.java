60636eab1f1b0749a2c8328b102fda6b96f2bbbc
George G <kreach3r@users.noreply.github.com>
2016-07-02 22:01:03 +0300

    InstalledApp: show link to Google Play
    Shows a Google Play store icon in the App Info actionbar of apps
    Excludes aosp built ones, but still works for apps generally found in /system, like SuperSU
    The openPlayStore() code was created by @nicholaschum for Substratum, thank you!
    
    BenzoEdit: fixed it for pie and updated drawable
    
    Change-Id: Ic55e572405d97b1f8ae3f8e3b5aceaa38b6a991e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -34,6 +34,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
+import android.graphics.drawable.Drawable;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.net.Uri;
@@ -55,6 +56,7 @@ import androidx.annotation.VisibleForTesting;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 import com.android.settings.applications.manageapplications.ManageApplications;
 import com.android.settings.applications.specialaccess.interactacrossprofiles.InteractAcrossProfilesDetailsPreferenceController;
 import com.android.settings.applications.specialaccess.pictureinpicture.PictureInPictureDetailPreferenceController;
@@ -93,6 +95,7 @@ public class AppInfoDashboardFragment extends DashboardFragment
     static final int UNINSTALL_UPDATES = 2;
     static final int INSTALL_INSTANT_APP_MENU = 3;
     static final int ACCESS_RESTRICTED_SETTINGS = 4;
+    public static final int PLAY_STORE = 5;
 
     // Result code identifiers
     @VisibleForTesting
@@ -400,6 +403,9 @@ public class AppInfoDashboardFragment extends DashboardFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+        menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
+                .setIcon(R.drawable.ic_menu_play_store)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(0, UNINSTALL_UPDATES, 0, R.string.app_factory_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(0, UNINSTALL_ALL_USERS_MENU, 1, R.string.uninstall_all_users_text)
@@ -435,6 +441,10 @@ public class AppInfoDashboardFragment extends DashboardFragment
             RestrictedLockUtilsInternal.setMenuItemAsDisabledByAdmin(getActivity(),
                     uninstallUpdatesItem, mAppsControlDisallowedAdmin);
         }
+        // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
+        // and grab the Google Play Store itself (com.android.vending) in the process
+        menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
+                && !isAospOrStore(mAppEntry.info.packageName));
     }
 
     /** Shows the lock screen if the keyguard is secured. */
@@ -502,6 +512,9 @@ public class AppInfoDashboardFragment extends DashboardFragment
                             mAppEntry.label);
                     Toast.makeText(getContext(), toastString, Toast.LENGTH_LONG).show();
                 });
+                return true;
+            case PLAY_STORE:
+                openPlayStore(mAppEntry.info.packageName);
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -612,6 +625,17 @@ public class AppInfoDashboardFragment extends DashboardFragment
     @Override
     protected boolean shouldSkipForInitialSUW() {
         return true;
+    }
+
+    private void openPlayStore(String packageName) {
+        // Launch an intent to the play store entry
+        String playURL = "https://play.google.com/store/apps/details?id=" + packageName;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(playURL));
+        startActivity(i);
+    }
+     private boolean isAospOrStore(String packageName) {
+        return packageName.contains("com.android");
     }
 
     private void uninstallPkg(String packageName, boolean allUsers, boolean andDisable) {
