b697c0008946087d015d19eedd9f430ee7959fe5
Luis Vidal <lvidal@cyngn.com>
2016-08-05 12:54:12 -0700

    Settings: Animate pin/pattern fragment only if available
    To prevent NPE, check if the PIN/Pattern/Password confirmation
    fragment is available before performing animation
    
    Change-Id: I4ae78569f953daa6696e96c8318a0c059aaf3502
    TICKET: HAM-1397
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/ConfirmDeviceCredentialBaseActivity.java
+++ src/com/android/settings/ConfirmDeviceCredentialBaseActivity.java
@@ -110,10 +110,16 @@ public abstract class ConfirmDeviceCredentialBaseActivity extends SettingsActivi
     }
 
     public void prepareEnterAnimation() {
-        getFragment().prepareEnterAnimation();
+        final ConfirmDeviceCredentialBaseFragment f = getFragment();
+        if (f != null) {
+            f.prepareEnterAnimation();
+        }
     }
 
     public void startEnterAnimation() {
-        getFragment().startEnterAnimation();
+        final ConfirmDeviceCredentialBaseFragment f = getFragment();
+        if (f != null) {
+            f.startEnterAnimation();
+        }
     }
 }
