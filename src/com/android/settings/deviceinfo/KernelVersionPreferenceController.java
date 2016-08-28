7b47f26e9585df19bf30c11724ae7b14dcfc2687
xoomdev <xoomdevnet@gmail.com>
2016-08-27 22:49:50 -0400

    Show full /proc/version information
    - Adapt to N
    
    Change-Id: Id0d04e43ce5c3ac7af81a7ad19f4c6c375d94b82
    [MSF-Jarvis: Forward port to Oreo]
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    [nathanchance: Clean up and make it work :P]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/KernelVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/KernelVersionPreferenceController.java
@@ -15,8 +15,14 @@
  */
 package com.android.settings.deviceinfo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import android.content.Context;
 import android.support.v7.preference.Preference;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.DeviceInfoUtils;
@@ -26,6 +32,8 @@ public class KernelVersionPreferenceController extends AbstractPreferenceControl
         PreferenceControllerMixin {
 
     private static final String KEY_KERNEL_VERSION = "kernel_version";
+    private static final String FILENAME_PROC_VERSION = "/proc/version";
+    private static final String LOG_TAG = "KernelVersionPreferenceController";
 
     public KernelVersionPreferenceController(Context context) {
         super(context);
@@ -45,5 +53,41 @@ public class KernelVersionPreferenceController extends AbstractPreferenceControl
     @Override
     public String getPreferenceKey() {
         return KEY_KERNEL_VERSION;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), KEY_KERNEL_VERSION)) {
+            return false;
+        }
+        preference.setSummary(getFullKernelVersion());
+        return false;
+    }
+
+    private String getFullKernelVersion() {
+        String procVersionStr;
+        try {
+            procVersionStr = readLine(FILENAME_PROC_VERSION);
+            return procVersionStr;
+        } catch (IOException e) {
+            Log.e(LOG_TAG,
+            "IO Exception when getting kernel version for Device Info screen", e);
+            return "Unavailable";
+        }
+    }
+
+    /**
+     * Reads a line from the specified file.
+     * @param filename the file to read from
+     * @return the first line, if any.
+     * @throws IOException if the file couldn't be read
+     */
+    private static String readLine(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
     }
 }
