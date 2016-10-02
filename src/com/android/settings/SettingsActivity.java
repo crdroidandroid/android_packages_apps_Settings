0656250b8be93aa4d1cab00a7f142c45a9f54c6d
Pranav Vashi <neobuddy89@gmail.com>
2016-10-02 13:05:57 +0530

    settings: Initial prep for crDroid Settings
    Change-Id: I3fa033448acb7e6e205bade3160678f075c24793
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -135,6 +135,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
+import com.crdroid.settings.crDroidSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -291,6 +292,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             UserDictionaryList.class.getName(),
             UserDictionarySettings.class.getName(),
             HomeSettings.class.getName(),
+            crDroidSettings.class.getName(),
             DisplaySettings.class.getName(),
             DeviceInfoSettings.class.getName(),
             ManageApplications.class.getName(),
