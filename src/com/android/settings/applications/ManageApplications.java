fe9347b9f75743cbdf0ba3099835cc74dbe6e1d1
Ivan Iskandar <iiiiskandar14@gmail.com>
2016-12-14 14:58:54 +0100

    Hide the "show/hide overlays" when no overlay installed
    Change-Id: I54088d27c9492e0992992de59bf340fe2465260a
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -196,6 +196,10 @@ public class ManageApplications extends InstrumentedFragment
     private boolean mShowSubstratum;
     private boolean mShowSubstratumIcons;
 
+    // if app and icon overlay installed
+    private boolean mAppOverlayInstalled;
+    private boolean mIconOverlayInstalled;
+
     private ApplicationsState mApplicationsState;
 
     public int mListType;
@@ -558,6 +562,9 @@ public class ManageApplications extends InstrumentedFragment
     }
 
     void updateOptionsMenu() {
+        mAppOverlayInstalled = isOverlayInstalled("app");
+        mIconOverlayInstalled = isOverlayInstalled("icon");
+
         if (mOptionsMenu == null) {
             return;
         }
@@ -575,13 +582,13 @@ public class ManageApplications extends InstrumentedFragment
                 && mListType != LIST_TYPE_HIGH_POWER);
 
         mOptionsMenu.findItem(R.id.show_substratum).setVisible(!mShowSubstratum
-                && mListType != LIST_TYPE_HIGH_POWER);
+                && mListType != LIST_TYPE_HIGH_POWER && mAppOverlayInstalled);
         mOptionsMenu.findItem(R.id.hide_substratum).setVisible(mShowSubstratum
-                && mListType != LIST_TYPE_HIGH_POWER);
+                && mListType != LIST_TYPE_HIGH_POWER && mAppOverlayInstalled);
         mOptionsMenu.findItem(R.id.show_substratum_icons).setVisible(!mShowSubstratumIcons
-                && mListType != LIST_TYPE_HIGH_POWER);
+                && mListType != LIST_TYPE_HIGH_POWER && mIconOverlayInstalled);
         mOptionsMenu.findItem(R.id.hide_substratum_icons).setVisible(mShowSubstratumIcons
-                && mListType != LIST_TYPE_HIGH_POWER);
+                && mListType != LIST_TYPE_HIGH_POWER && mIconOverlayInstalled);
     }
 
     @Override
@@ -668,6 +675,29 @@ public class ManageApplications extends InstrumentedFragment
         }
         mFilterAdapter.setFilterEnabled(FILTER_APPS_ENABLED, hasDisabledApps);
         mFilterAdapter.setFilterEnabled(FILTER_APPS_DISABLED, hasDisabledApps);
+    }
+
+    boolean isOverlayInstalled(String type) {
+        List<ApplicationInfo> packages = getActivity().getPackageManager()
+                .getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            if (packageInfo.metaData != null) {
+                if (type.equals("app")) {
+                    if (packageInfo.metaData
+                                    .getString("Substratum_Parent") != null) {
+                        return true;
+                    }
+                }
+                if (type.equals("icon")) {
+                    if (packageInfo.metaData
+                                    .getString("Substratum_IconPack") != null) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @Override
