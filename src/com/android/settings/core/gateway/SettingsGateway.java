a51c93623da18522c93eb71c1798d4df00827129
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Settings: Allow to quickly open Running Services from meminfo view
    @neobuddy89: Updated for A14.
    
    Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -46,6 +46,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -405,6 +406,7 @@ public class SettingsGateway {
             ZenModesListFragment.class.getName(),
             ZenModeFragment.class.getName(),
             crDroidSettingsLayout.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -452,5 +454,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }
