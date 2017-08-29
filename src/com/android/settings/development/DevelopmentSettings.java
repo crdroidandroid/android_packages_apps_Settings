ec5b3a73fbfeceed0a3d211742223bef8e061703
beanstown106 <nbenis106@gmail.com>
2017-08-29 22:26:47 +0200

    Enable Development Settings By Default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -3031,7 +3031,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
                     return context.getSharedPreferences(DevelopmentSettings.PREF_FILE,
                             Context.MODE_PRIVATE).getBoolean(
                             DevelopmentSettings.PREF_SHOW,
-                            android.os.Build.TYPE.equals("eng"));
+                            android.os.Build.TYPE.equals("eng") || android.os.Build.TYPE.equals("userdebug")
+                            || android.os.Build.TYPE.equals("user"));
                 }
 
                 @Override
