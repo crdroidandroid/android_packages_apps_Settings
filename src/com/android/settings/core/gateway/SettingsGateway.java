19d6bf501b850ba789eb3830f8bd951af3f14c4d
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate crDroidSettings into Settings
    Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -217,6 +217,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.crdroid.settings.crDroidSettingsLayout;
+
 public class SettingsGateway {
 
     /**
@@ -422,6 +424,7 @@ public class SettingsGateway {
             ContentProtectionPreferenceFragment.class.getName(),
             ToggleScreenMagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
+            crDroidSettingsLayout.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -468,5 +471,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.crDroidSettingsLayoutActivity.class.getName(),
     };
 }
