fd8e272b9d1fd2d8fbdc71cf19a5dca7a9ebc929
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate crDroidSettings into Settings
    Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -522,4 +522,6 @@ public class Settings extends SettingsActivity {
     public static class HearingDevicesActivity extends SettingsActivity { /* empty */ }
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
     public static class ContactsStorageSettingsActivity extends SettingsActivity { /* empty */ }
+
+    public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
 }
