7573e95297a4e2f3ce9893172c98a834b31e0c5a
Pranav Vashi <neobuddy89@gmail.com>
2017-09-14 19:00:19 +0530

    Settings: Integrate crDroid settings
    Change-Id: I902bfda3cc803b3a10324400120df489ea20320b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,4 +178,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
 
+    public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
 }
