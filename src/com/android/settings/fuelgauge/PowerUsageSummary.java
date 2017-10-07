9124ef8009f6fa3cf428793ca232cf27dc366788
Alex Cruz <du.alexcruz@gmail.com>
2017-10-07 17:55:47 -0400

    Remove extra preference for Auto Brightness
    This like the 'screen timeout' under Battery just doesn't make sense.
    Most users have got used to having these options under Settings/Display.
    
    Change-Id: I568475ef0023ac256bbee046758d0df4a0fadd00
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -96,7 +96,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
 
-    private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness_battery";
+    //private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness_battery";
     //private static final String KEY_SCREEN_TIMEOUT = "screen_timeout_battery";
     private static final String KEY_AMBIENT_DISPLAY = "ambient_display_battery";
     private static final String KEY_BATTERY_SAVER_SUMMARY = "battery_saver_summary";
@@ -308,7 +308,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         mBatteryHeaderPreferenceController = new BatteryHeaderPreferenceController(
                 context, getActivity(), this /* host */, getLifecycle());
         controllers.add(mBatteryHeaderPreferenceController);
-        controllers.add(new AutoBrightnessPreferenceController(context, KEY_AUTO_BRIGHTNESS));
+        //controllers.add(new AutoBrightnessPreferenceController(context, KEY_AUTO_BRIGHTNESS));
         //controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new BatterySaverController(context, getLifecycle()));
         controllers.add(new BatteryPercentagePreferenceController(context));
@@ -917,7 +917,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
                     niks.add(KEY_HIGH_USAGE);
                     niks.add(KEY_BATTERY_SAVER_SUMMARY);
                     // Duplicates in display
-                    niks.add(KEY_AUTO_BRIGHTNESS);
+                    //niks.add(KEY_AUTO_BRIGHTNESS);
                     //niks.add(KEY_SCREEN_TIMEOUT);
                     niks.add(KEY_AMBIENT_DISPLAY);
                     return niks;
