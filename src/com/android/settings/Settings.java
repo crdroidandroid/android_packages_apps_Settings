4534dc1187e00b0d58f1914a55c26428daf92ab5
bigrushdog <randall.rushing@gmail.com>
2017-11-14 01:30:47 -0800

    DUI: Initial checkin for Oreo [3/7]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -193,4 +193,8 @@ public class Settings extends SettingsActivity {
 
     public static class crDroidSettingsActivity extends SettingsActivity {}
     public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
+    public static class NavigationSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class FlingSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
 }
