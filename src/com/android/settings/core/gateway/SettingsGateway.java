DarkUnicornss/settings

DUI: Initial checkin for Oreo [3/7]
Change-Id: I5ea7016c221c0e85752180cd784ec56f715e2c02

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -129,6 +129,10 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.dirtyunicorns.tweaks.DirtyTweaks;
+import com.dirtyunicorns.tweaks.fragments.FlingSettings;
+import com.dirtyunicorns.tweaks.fragments.NavigationBar;
+import com.dirtyunicorns.tweaks.fragments.PulseSettings;
+import com.dirtyunicorns.tweaks.fragments.SmartbarSettings;
 
 public class SettingsGateway {
 
@@ -249,6 +253,10 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             DirtyTweaks.class.getName(),
+            NavigationBar.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -288,5 +296,9 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.DirtyTweaksActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName()
     };
 }

crdroidandroid/android_packages_apps_Settings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -134,6 +134,10 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.crdroid.settings.crDroidSettings;
 import com.crdroid.settings.crDroidSettingsLayout;
+import com.crdroid.settings.fragments.NavBar;
+import com.crdroid.settings.fragments.navbar.Fling;
+import com.crdroid.settings.fragments.navbar.Pulse;
+import com.crdroid.settings.fragments.navbar.Smartbar;
 
 public class SettingsGateway {
 
@@ -258,6 +262,10 @@ public class SettingsGateway {
             DataUsageList.class.getName(),
             crDroidSettings.class.getName(),
             crDroidSettingsLayout.class.getName(),
+            NavBar.class.getName(),
+            Fling.class.getName(),
+            Pulse.class.getName(),
+            Smartbar.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -298,5 +306,9 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.crDroidSettingsActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
     };
 }