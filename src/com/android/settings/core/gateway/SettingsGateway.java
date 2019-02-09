syberia-project/platform_packages_apps_Settings

Settings: PA PIE Controls 3.0 [2/3]
Change-Id: Ib5c028f2065cc102523a8261f3d63fa539626571
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.syberia.settings.SyberiaSettings;
 import com.syberia.settings.fragments.OmniJawsSettings;
+import com.syberia.settings.fragments.PieSettings;
 
 public class SettingsGateway {
 
@@ -277,7 +278,8 @@ public class SettingsGateway {
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName(),
-            RunningServices.class.getName()
+            RunningServices.class.getName(),
+            PieSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,6 +324,7 @@ public class SettingsGateway {
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(), 
             Settings.PulseSettingsActivity.class.getName(),
-            Settings.DevRunningServicesActivity.class.getName()
+            Settings.DevRunningServicesActivity.class.getName(),
+            Settings.PieSettingsActivity.class.getName()
     };
 }

crdroidandroid/android_packages_apps_Settings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -24,6 +24,7 @@ import com.android.settings.MasterClear;
 import com.android.settings.PrivacySettings;
 import com.android.settings.Settings;
 import com.android.settings.Settings.FlingSettingsActivity;
+import com.android.settings.Settings.PieSettingsActivity;
 import com.android.settings.Settings.PulseSettingsActivity;
 import com.android.settings.Settings.SmartbarSettingsActivity;
 import com.android.settings.TestingSettings;
@@ -271,6 +272,7 @@ public class SettingsGateway {
             crDroidSettingsLayout.class.getName(),
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
+            PieSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName()
     };
 
@@ -312,7 +314,8 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
-            Settings.FlingSettingsActivity.class.getName(), 
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.PieSettingsActivity.class.getName(),
             Settings.PulseSettingsActivity.class.getName()
     };
 }