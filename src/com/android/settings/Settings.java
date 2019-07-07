4814fdc69721721633b59a9fa842cc337ec34f94
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 11:56:32 +0530

    Fix long click intent for Smart Pixels tile [2/2]
    Change-Id: Id6b5fffd814f035be1bb75d1434456e0c02aa97f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -520,4 +520,6 @@ public class Settings extends SettingsActivity {
     public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
 
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+
+    public static class SmartPixelsActivity extends SettingsActivity {}
 }
