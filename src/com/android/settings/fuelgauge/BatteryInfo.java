b1911f126f9c5fc085de04a7c468291d9669e930
DennySPB <dennyspb@gmail.com>
2019-09-15 04:17:29 +0200

    Battery: show battery temp on power summary
    @neobuddy89:
    * Change layout to match other preferences
    * Fixed decimal value of battery temperature
    * Strings added under crdroid settings
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I868d12992ad44b018237bf9804aeb8cfe2f2ba2a

--- src/com/android/settings/fuelgauge/BatteryInfo.java
+++ src/com/android/settings/fuelgauge/BatteryInfo.java
@@ -46,6 +46,7 @@ public class BatteryInfo {
     public int batteryLevel;
     public boolean discharging = true;
     public boolean isOverheated;
+    static float batteryTemp;
     public long remainingTimeUs = 0;
     public long averageTimeToDischarge = EstimateKt.AVERAGE_TIME_TO_DISCHARGE_UNKNOWN;
     public String batteryPercentString;
@@ -236,6 +237,7 @@ public class BatteryInfo {
         info.isOverheated = batteryBroadcast.getIntExtra(
                 BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)
                 == BatteryManager.BATTERY_HEALTH_OVERHEAT;
+        info.batteryTemp = batteryBroadcast.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
 
         info.statusLabel = Utils.getBatteryStatus(context, batteryBroadcast);
         if (!info.mCharging) {
