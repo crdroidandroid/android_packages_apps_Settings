0ae3145f05d0e0d13a0b8909834625a618e3ed66
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 15:48:25 +0530

    Settings: Show CR version in About menu
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreferenceController.java
@@ -17,46 +17,21 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.os.SystemClock;
 import android.os.SystemProperties;
-import android.os.UserHandle;
-import android.os.UserManager;
-import android.text.TextUtils;
-import android.util.Log;
 
-import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
 import com.android.settings.R;
-import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
-import com.android.settingslib.RestrictedLockUtils;
-import com.android.settingslib.RestrictedLockUtilsInternal;
 
 public class LineageVersionDetailPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "lineageVersionDialogCtrl";
-    private static final int DELAY_TIMER_MILLIS = 500;
-    private static final int ACTIVITY_TRIGGER_COUNT = 3;
 
-    private static final String KEY_LINEAGE_VERSION_PROP = "ro.lineage.display.version";
-
-    private static final String PLATLOGO_PACKAGE_NAME = "org.lineageos.lineageparts";
-    private static final String PLATLOGO_ACTIVITY_CLASS =
-            PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity";
-
-    private final UserManager mUserManager;
-    private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
-
-    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
-    private boolean mFunDisallowedBySystem;
+    private static final String KEY_LINEAGE_VERSION_PROP = "ro.modversion";
 
     public LineageVersionDetailPreferenceController(Context context, String key) {
         super(context, key);
-        mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
-        initializeAdminPermissions();
     }
 
     @Override
@@ -82,48 +57,6 @@ public class LineageVersionDetailPreferenceController extends BasePreferenceCont
 
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
-        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
-            return false;
-        }
-        if (Utils.isMonkeyRunning()) {
-            return false;
-        }
-        arrayCopy();
-        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
-        if (mHits[0] >= (SystemClock.uptimeMillis() - DELAY_TIMER_MILLIS)) {
-            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
-                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
-                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
-                            mFunDisallowedAdmin);
-                }
-                Log.d(TAG, "Sorry, no fun for you!");
-                return true;
-            }
-
-            final Intent intent = new Intent(Intent.ACTION_MAIN)
-                     .setClassName(PLATLOGO_PACKAGE_NAME, PLATLOGO_ACTIVITY_CLASS);
-            try {
-                mContext.startActivity(intent);
-            } catch (Exception e) {
-                Log.e(TAG, "Unable to start activity " + intent.toString());
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Copies the array onto itself to remove the oldest hit.
-     */
-    @VisibleForTesting
-    void arrayCopy() {
-        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
-    }
-
-    @VisibleForTesting
-    void initializeAdminPermissions() {
-        mFunDisallowedAdmin = RestrictedLockUtilsInternal.checkIfRestrictionEnforced(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
-        mFunDisallowedBySystem = RestrictedLockUtilsInternal.hasBaseUserRestriction(
-                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+        return false;
     }
 }
