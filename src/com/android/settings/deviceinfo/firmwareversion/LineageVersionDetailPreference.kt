9c6da8d8e4acc3ff1fa595cd09a422fc89d003ba
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 15:48:25 +0530

    Settings: Show CR version in About menu
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/LineageVersionDetailPreference.kt
@@ -102,10 +102,10 @@ class LineageVersionDetailPreference :
         const val ACTIVITY_TRIGGER_COUNT = 3
         const val DELAY_TIMER_MILLIS = 500L
 
-        const val LINEAGE_VERSION_PROPERTY: String = "ro.lineage.version"
+        const val LINEAGE_VERSION_PROPERTY: String = "ro.crdroid.build.version"
 
-        const val PLATLOGO_PACKAGE_NAME: String = "org.lineageos.lineageparts"
-        const val PLATLOGO_ACTIVITY_CLASS: String = PLATLOGO_PACKAGE_NAME + ".logo.PlatLogoActivity"
+        const val PLATLOGO_PACKAGE_NAME: String = "com.crdroid.settings"
+        const val PLATLOGO_ACTIVITY_CLASS: String = PLATLOGO_PACKAGE_NAME + ".fragments.about.DonateActivity"
     }
 }
 // LINT.ThenChange(LineageVersionDetailPreferenceController.java)
