d3cbaae99b05b08b17e0c8039d009c16cd51e63d
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 02:30:00 +0530

    Bring back Gaming mode [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -179,5 +179,6 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
 
     public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
+    public static class GamingModeActivity extends SettingsActivity {}
     public static class SmartPixelsActivity extends SettingsActivity {}
 }
