3345f6d11c525aef027bddfe80a8fd268aa6c6d9
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-12-02 15:46:19 +0100

    Settings: Add Device codename to Firmware version window
    neobuddy89: Updated for A16 QPR1
    
    Squashed:
    
            From: Pranav Vashi <neobuddy89@gmail.com>
            Date: Fri, 17 Dec 2021 02:51:46 +0530
            Subject: Settings: Set device name from marketname if available
    
            From: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
            Date: Mon, 27 Dec 2021 14:09:31 +0200
            Subject: Fix device name change
    
            Change-Id: If974ac38638fca9b100008d1b5404d876faf42e2
            Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I61fd4d77375df019da94ed7fca71da157dd0f415
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -69,6 +69,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
     override fun getPreferenceHierarchy(context: Context, coroutineScope: CoroutineScope) =
         preferenceHierarchy(context) {
             +LogoPreference()
+            +AboutDeviceNamePreference()
             +FirmwareVersionDetailPreference()
             +LineageVersionDetailPreference()
             +SecurityPatchLevelPreference()
