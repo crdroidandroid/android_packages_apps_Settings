ea1c9482e4bd41d6d0d05993cfbad4c15d18cda8
Pranav Vashi <neobuddy89@gmail.com>
2019-12-25 22:31:40 +0530

    Revert "Settings: adaptive icon"
    This reverts commit 62f45d7d69750c7e0b926589022ce14c0d54d1e0.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -478,7 +478,7 @@ public class SettingsActivity extends SettingsBaseActivity
 
     @Override
     public void setTaskDescription(ActivityManager.TaskDescription taskDescription) {
-        taskDescription.setIcon(R.mipmap.ic_launcher);
+        taskDescription.setIcon(R.drawable.ic_launcher_settings);
         super.setTaskDescription(taskDescription);
     }
 
