19567be9295d9218b3049d5520142189df8ff0fc
rituj <ritujbeniwal@gmail.com>
2020-07-27 18:23:45 +0530

    Settings: Add option to cycle through ringer modes [2/3]
     * @mickaelmendes50 : Adapt to Android 12, 13
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: If4a387a9f8a465d33b25b87b0360fa20f1cee7de

--- src/com/android/settings/gestures/PreventRingingParentPreferenceController.java
+++ src/com/android/settings/gestures/PreventRingingParentPreferenceController.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.gestures;
 
+import static android.provider.Settings.Secure.VOLUME_HUSH_CYCLE;
 import static android.provider.Settings.Secure.VOLUME_HUSH_GESTURE;
 import static android.provider.Settings.Secure.VOLUME_HUSH_MUTE;
 import static android.provider.Settings.Secure.VOLUME_HUSH_VIBRATE;
@@ -101,6 +102,9 @@ public class PreventRingingParentPreferenceController extends TogglePreferenceCo
                     break;
                 case VOLUME_HUSH_MUTE:
                     summary = mContext.getText(R.string.prevent_ringing_option_mute_summary);
+                    break;
+                case VOLUME_HUSH_CYCLE:
+                    summary = mContext.getText(R.string.prevent_ringing_option_cycle_summary);
                     break;
                 // VOLUME_HUSH_OFF
                 default:
