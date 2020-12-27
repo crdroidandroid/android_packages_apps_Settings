a007f0fc4084fac585ba396e92d6c6e9e0396163
TheScarastic <warabhishek@gmail.com>
2020-12-27 11:28:59 +0300

    Settings: Add screen off fod [2/2]
    Change-Id: I7d443192a1929630d9dbe409f23c9aaff0e4c542
    12

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -83,6 +83,7 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new QuickUnlockPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
+        controllers.add(new ScreenOffFodPreferenceController(context));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         return controllers;
     }
