2b4ef4b9b9abf67f6fdd529e8aa6d87b4a2a78b3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 21:14:30 -0300

    Port faceunlock feature
    From motorola/nio_retcn/nio:11/RRN31.Q1-20-17-1/1cbc0:user/release-keys
    
    Change-Id: I686b5128405426274b4176d9c09192a6790c82f9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/biometrics/face/FaceSettings.java
+++ src/com/android/settings/biometrics/face/FaceSettings.java
@@ -49,6 +49,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.android.settings.custom.biometrics.FaceUtils;
+import com.android.settings.custom.biometrics.face.FaceSettingsRedoPreferenceController;
+
 /**
  * Settings screen for face authentication.
  */
@@ -71,6 +74,7 @@ public class FaceSettings extends DashboardFragment {
     private FaceSettingsRemoveButtonPreferenceController mRemoveController;
     private FaceSettingsEnrollButtonPreferenceController mEnrollController;
     private FaceSettingsLockscreenBypassPreferenceController mLockscreenController;
+    private FaceSettingsRedoPreferenceController mRedoController;
     private List<AbstractPreferenceController> mControllers;
 
     private List<Preference> mTogglePreferences;
@@ -81,6 +85,12 @@ public class FaceSettings extends DashboardFragment {
     private boolean mConfirmingPassword;
 
     private final FaceSettingsRemoveButtonPreferenceController.Listener mRemovalListener = () -> {
+        if (FaceUtils.isFaceUnlockSupported()){
+            if (getActivity() != null) {
+                getActivity().finish();
+            }
+            return;
+        }
 
         // Disable the toggles until the user re-enrolls
         for (Preference preference : mTogglePreferences) {
@@ -177,6 +187,10 @@ public class FaceSettings extends DashboardFragment {
             }
         }
         mRemoveController.setUserId(mUserId);
+
+        if (mRedoController != null) {
+            mRedoController.setUserId(mUserId);
+        }
 
         // Don't show keyguard controller for work profile settings.
         if (mUserManager.isManagedProfile(mUserId)) {
@@ -300,6 +314,9 @@ public class FaceSettings extends DashboardFragment {
                 mEnrollController = (FaceSettingsEnrollButtonPreferenceController) controller;
                 mEnrollController.setListener(mEnrollListener);
                 mEnrollController.setActivity((SettingsActivity) getActivity());
+            } else if (controller instanceof FaceSettingsRedoPreferenceController) {
+                mRedoController = (FaceSettingsRedoPreferenceController) controller;
+                mRedoController.setActivity((SettingsActivity) getActivity());
             }
         }
 
@@ -316,6 +333,7 @@ public class FaceSettings extends DashboardFragment {
         controllers.add(new FaceSettingsFooterPreferenceController(context));
         controllers.add(new FaceSettingsConfirmPreferenceController(context));
         controllers.add(new FaceSettingsEnrollButtonPreferenceController(context));
+        controllers.add(new FaceSettingsRedoPreferenceController(context));
         return controllers;
     }
 
