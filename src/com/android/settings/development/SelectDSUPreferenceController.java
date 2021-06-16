9027f28ed473a8a08ca18ed9a45edadd13e3404d
Joey Huab <joey@evolution-x.org>
2021-06-16 16:02:58 +0900

    Settings: Remove DSU Loader if unsupported
    * Based on https://github.com/yaap/packages_apps_Settings/commit/605b1f680a3f71c94c8966a49db3dab72204efdc.

--- src/com/android/settings/development/SelectDSUPreferenceController.java
+++ src/com/android/settings/development/SelectDSUPreferenceController.java
@@ -38,6 +38,11 @@ class SelectDSUPreferenceController extends DeveloperOptionsPreferenceController
         return DSU_LOADER_KEY;
     }
 
+    @Override
+    public boolean isAvailable() {
+        return SystemProperties.getBoolean("ro.boot.dynamic_partitions", false);
+    }
+
     private boolean isDSURunning() {
         return SystemProperties.getBoolean("ro.gsid.image_running", false);
     }
