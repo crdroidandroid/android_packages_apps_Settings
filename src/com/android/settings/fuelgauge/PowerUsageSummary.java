6e3c66baab1391bf81d495aea3643d0b4dae7022
Pranav Vashi <neobuddy89@gmail.com>
2021-08-30 01:55:13 +0530

    BatteryHealth: Make layout more consistent
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I768562f980f4711f2889b4cc55d8d6b54930daf8

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -413,9 +413,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 mStatsHelper.getStats(), elapsedRealtimeUs, false);
         updateHeaderPreference(batteryInfo);
         if (mBatteryHealthSupported) {
-            mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity)));
-            mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(getResources().getString(R.string.config_batteryDesignCapacity)));
-            mBatteryChargeCycles.setSubtitle(parseBatteryCycle(getResources().getString(R.string.config_batteryChargeCycles)));
+            mCurrentBatteryCapacity.setSummary(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity)));
+            mDesignedBatteryCapacity.setSummary(parseBatterymAhText(getResources().getString(R.string.config_batteryDesignCapacity)));
+            mBatteryChargeCycles.setSummary(parseBatteryCycle(getResources().getString(R.string.config_batteryChargeCycles)));
         }
     }
 
