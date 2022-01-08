84f81124b38f6b69decc6dc6964f96a76d49c11c
jhonboy121 <alfredmathew05@gmail.com>
2022-01-08 15:52:47 +0530

    Settings: Fix rest of the edit menu icon tint
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/wifi/details/WifiNetworkDetailsFragment.java
+++ src/com/android/settings/wifi/details/WifiNetworkDetailsFragment.java
@@ -23,6 +23,7 @@ import android.app.Dialog;
 import android.app.admin.DevicePolicyManager;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
@@ -213,6 +214,9 @@ public class WifiNetworkDetailsFragment extends RestrictedDashboardFragment impl
         if (!mIsUiRestricted && isEditable()) {
             MenuItem item = menu.add(0, Menu.FIRST, 0, R.string.wifi_modify);
             item.setIcon(com.android.internal.R.drawable.ic_mode_edit);
+            item.setIconTintList(ColorStateList.valueOf(
+                com.android.settingslib.Utils.getColorAttrDefaultColor(getContext(),
+                    android.R.attr.colorControlNormal)));
             item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         }
         super.onCreateOptionsMenu(menu, inflater);
