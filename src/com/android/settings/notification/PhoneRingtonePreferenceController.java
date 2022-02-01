53b22d8133ab4bd21db2736e7b957ca7483dae39
ShevT <evgeny.shishkov@gmail.com>
2022-02-01 22:00:14 +0300

    Revert all patches "Phone ringtone setting for Multi SIM device"
    Revert "MSIM Ringtone: Improve on devices with ESIM"
    
    This reverts commit 4662aa56eff6cc81e6d44483446f6499f06c11e7.
    
    Revert "Settings: Use AOSP translations for MSIM ringtone"
    
    This reverts commit ef0d1884aed61f07ed77bb0a96f1fa1f0808b47b.
    
    Revert "Settings: enable SIM card ringtone only if slot not empty"
    
    This reverts commit c6b32cfd0ba7f0e6c66ee1e5666874dedf82a929.
    
    Revert "Settings: Phone ringtone setting for Multi SIM device [2/3]"
    
    This reverts commit 51df714d5f9a8204f25a91a930a73a046181363b.

--- src/com/android/settings/notification/PhoneRingtonePreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtonePreferenceController.java
@@ -19,36 +19,14 @@ package com.android.settings.notification;
 import android.content.Context;
 import android.media.RingtoneManager;
 
-import android.telephony.TelephonyManager;
-
-import androidx.preference.PreferenceScreen;
-
-import com.android.settings.DefaultRingtonePreference;
-import com.android.settings.R;
-
 import com.android.settings.Utils;
 
 public class PhoneRingtonePreferenceController extends RingtonePreferenceControllerBase {
 
-    private static final int SLOT_ID = 0;
     private static final String KEY_PHONE_RINGTONE = "phone_ringtone";
 
     public PhoneRingtonePreferenceController(Context context) {
         super(context);
-    }
-
-    @Override
-    public void displayPreference(PreferenceScreen screen) {
-        super.displayPreference(screen);
-
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        if (telephonyManager.isMultiSimEnabled() && !isDeviceSupportsESIM()) {
-            DefaultRingtonePreference ringtonePreference =
-                    (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE);
-            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title));
-            ringtonePreference.setEnabled(hasCard());
-        }
     }
 
     @Override
@@ -64,11 +42,5 @@ public class PhoneRingtonePreferenceController extends RingtonePreferenceControl
     @Override
     public int getRingtoneType() {
         return RingtoneManager.TYPE_RINGTONE;
-    }
-
-    private boolean hasCard() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        return telephonyManager.hasIccCard(SLOT_ID);
     }
 }
