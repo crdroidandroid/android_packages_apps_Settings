71b168096be1d228ad575224e9b828fe14d7a0e4
jhenrique09 <jhsv09@gmail.com>
2022-02-10 22:27:30 -0300

    Initial import of Face Unlock for T
    Change-Id: I178b63d73f14742cf890cb05386ad4e82fd8c2be
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/homepage/contextualcards/FaceReEnrollDialog.java
+++ src/com/android/settings/homepage/contextualcards/FaceReEnrollDialog.java
@@ -33,6 +33,8 @@ import com.android.settings.Utils;
 import com.android.settings.biometrics.face.FaceUpdater;
 import com.android.settings.homepage.contextualcards.slices.FaceSetupSlice;
 
+import com.android.settings.custom.biometrics.FaceUtils;
+
 /**
  * This class is used to show a popup dialog for {@link FaceSetupSlice}.
  */
@@ -42,6 +44,7 @@ public class FaceReEnrollDialog extends AlertActivity implements
     private static final String TAG = "FaceReEnrollDialog";
 
     private static final String BIOMETRIC_ENROLL_ACTION = "android.settings.BIOMETRIC_ENROLL";
+    private static final String BIOMETRIC_ENROLL_ACTION_CUSTOM = "com.android.settings.intent.action.FACE_ENROLL";
 
     private FaceManager mFaceManager;
     private FaceUpdater mFaceUpdater;
@@ -113,7 +116,9 @@ public class FaceReEnrollDialog extends AlertActivity implements
                     return;
                 }
                 // Send user to the enroll flow.
-                final Intent reEnroll = new Intent(BIOMETRIC_ENROLL_ACTION);
+                final Intent reEnroll = new Intent(
+                    FaceUtils.isFaceUnlockSupported() ?
+                    BIOMETRIC_ENROLL_ACTION_CUSTOM : BIOMETRIC_ENROLL_ACTION);
                 final Context context = getApplicationContext();
 
                 try {
