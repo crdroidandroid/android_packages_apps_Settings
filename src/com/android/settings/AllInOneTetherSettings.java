2e704714ed767197bd6a49f8557b60a84bc6f606
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-05 00:20:16 +0800

    SoftAp: add more bands to band preference [1/2]
    Bands added:
    - 6 GHz
    - 60 GHz
    - 2.4 & 5 GHz
    - 2.4 & 6 GHz
    
    Change-Id: Id25de36811c6da34f11c51cf8cc85be3b53cc72f

--- src/com/android/settings/AllInOneTetherSettings.java
+++ src/com/android/settings/AllInOneTetherSettings.java
@@ -395,7 +395,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                     mPasswordPreferenceController.getPasswordValidated(securityType),
                     SoftApConfiguration.SECURITY_TYPE_WPA2_PSK);
         }
-        configBuilder.setBand(mApBandPreferenceController.getBandIndex());
+        mApBandPreferenceController.setupBands(configBuilder);
         mClientPreferenceController.updateConfig(configBuilder);
         configBuilder.setHiddenSsid(mHiddenSsidPreferenceController.isHiddenSsidEnabled());
         return configBuilder.build();
