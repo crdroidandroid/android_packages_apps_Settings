c5001bd51b7c906a2a30ea280757f1c437014633
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-10 13:25:01 +0800

    AppVolumeSlice: Allow querying icon of apps installed by any user
    Test: Launch an app from work profile, play some music and see
          the app icon can now be properly displayed in the panel.
    Change-Id: I76db52d78ddf0850a4229b5bd3557829d0cdbf98
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/media/AppVolumeSlice.java
+++ src/com/android/settings/media/AppVolumeSlice.java
@@ -110,7 +110,7 @@ public class AppVolumeSlice implements CustomSliceable {
     private IconCompat getApplicationIcon(String packageName) {
         PackageManager pm = mContext.getPackageManager();
         try {
-            ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, PackageManager.MATCH_ANY_USER);
             Resources resources = pm.getResourcesForApplication(ai);
             IconCompat icon = IconCompat.createWithResource(resources, packageName, ai.icon);
             return icon;
