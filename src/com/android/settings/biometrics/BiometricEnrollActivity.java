cc54bf9f7833ea1c73968adb2521aaca9d381cb3
jhenrique09 <jhsv09@gmail.com>
2022-03-20 21:18:13 +0000

    Settings: Disable face enroll on Setup Wizard
    Change-Id: I52b284de48f40965096e668d1e80198dcc966c2b

--- src/com/android/settings/biometrics/BiometricEnrollActivity.java
+++ src/com/android/settings/biometrics/BiometricEnrollActivity.java
@@ -57,6 +57,8 @@ import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import java.util.List;
 
+import com.android.settings.custom.biometrics.FaceUtils;
+
 /**
  * Trampoline activity launched by the {@code android.settings.BIOMETRIC_ENROLL} action which
  * shows the user an appropriate enrollment flow depending on the device's biometric hardware.
@@ -227,6 +229,9 @@ public class BiometricEnrollActivity extends InstrumentedActivity {
                     Log.d(TAG, "config_suw_support_face_enroll: " + mIsFaceEnrollable);
                 }
             }
+        }
+        if (FaceUtils.isFaceUnlockSupported() && isSetupWizard){
+            mIsFaceEnrollable = false;
         }
         if (mHasFeatureFingerprint) {
             final FingerprintManager fpManager = getSystemService(FingerprintManager.class);
