e4638b0f79745947f036ee3eab7437420b0740b7
Pranav Vashi <neobuddy89@gmail.com>
2022-03-29 17:45:37 +0530

    Revert "Settings: Import GoogleGameDashboard"
    This reverts commit 3f52ddadda84263d87d5056fbeba17ba4b1650bb.

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,16 +1,4 @@
 package com.google.android.settings.overlay;
 
-import com.android.settings.applications.GameSettingsFeatureProvider;
-import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
-
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
-    private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
-
-    @Override
-    public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
-        if (mGameSettingsFeatureProvider == null) {
-            mGameSettingsFeatureProvider = new GameSettingsFeatureProviderGoogleImpl();
-        }
-        return mGameSettingsFeatureProvider;
-    }
 }
