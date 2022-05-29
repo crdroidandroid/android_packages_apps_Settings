357dc98a62672a013b84d09c2d47044203e71b87
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-30 02:15:47 +0530

    Settings: Fix loading iconpack-based Wellbeing icon
    Wellbeing apk still provides Android 11 style settings icon
    when an icon pack is set. It consists of a layer-list with
    circle shape and drawable, so fetch the drawable to make
    it work with Android 12 settings.
    
    Change-Id: I78be2db9dc8ca262e61fdb13b93da007c3a6eb81
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
@@ -100,6 +100,7 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
     private static final String DASHBOARD_TILE_PREF_KEY_PREFIX = "dashboard_tile_pref_";
     private static final String META_DATA_KEY_INTENT_ACTION = "com.android.settings.intent.action";
     private static final String TOP_LEVEL_ACCOUNT_CATEGORY = "top_level_account_category";
+    private static final String WELLBEING_PACKAGE = "com.google.android.apps.wellbeing";
 
     @VisibleForTesting
     enum ColorScheme {
@@ -543,6 +544,11 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
                     preference.setIcon(iconDrawable);
                     return;
                 }
+            }
+            if (iconPackage.equals(WELLBEING_PACKAGE) && iconDrawable instanceof LayerDrawable
+                    && ((LayerDrawable) iconDrawable).getDrawable(1) != null) {
+                iconDrawable = ((LayerDrawable) iconDrawable).getDrawable(1);
+                iconDrawable.mutate();
             }
             iconDrawable.setTint(Utils.getHomepageIconColor(preference.getContext()));
         }
