13608a355e3739ca1dc658b1b15816002870479a
jhenrique09 <jhsv09@gmail.com>
2022-06-07 16:04:37 +0000

    Settings: Fix ringtone selection for devices with e-SIM
    https://github.com/PixelExperience/android-issues/issues/2651
    
    Change-Id: Ib283876196346b3b5492b8b4403530227cdf961e
    (cherry picked from commit 4b275f3f518acc939a73523569602f6112737fc0)
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/notification/PhoneRingtone2PreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtone2PreferenceController.java
@@ -43,8 +43,14 @@ public class PhoneRingtone2PreferenceController extends RingtonePreferenceContro
         DefaultRingtonePreference ringtonePreference =
                 (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE2);
         ringtonePreference.setSlotId(SLOT_ID);
-        ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " - " +
-            String.format(mContext.getString(R.string.sim_card_number_title), 2));
+
+        if (isBuiltInEuiccSlot(SLOT_ID)) {
+            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " (e-SIM)");
+        } else {
+            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " - " +
+                String.format(mContext.getString(R.string.sim_card_number_title), 2));
+        }
+
         ringtonePreference.setEnabled(hasCard());
     }
 
@@ -55,9 +61,6 @@ public class PhoneRingtone2PreferenceController extends RingtonePreferenceContro
 
     @Override
     public boolean isAvailable() {
-        if (isBuiltInEuiccSlot(SLOT_ID)) {
-            return false;
-        }
         TelephonyManager telephonyManager =
                 (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
         return Utils.isVoiceCapable(mContext) && telephonyManager.isMultiSimEnabled();
