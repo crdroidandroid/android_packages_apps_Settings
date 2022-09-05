b6ec95050dedd7244f7e92279ec23e659f314571
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-09-05 04:43:13 +0000

    Settings: support per-app volume [3/3]
    Change-Id: I3e72a4b0fda68a5cd1e9a35fc161a1d7ddae6d7e
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/panel/PanelFeatureProviderImpl.java
+++ src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -87,6 +87,8 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                         return VolumePanel.create(context);
                     }
                 }
+            case Settings.Panel.ACTION_APP_VOLUME:
+                return AppVolumePanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: " + panelType);
