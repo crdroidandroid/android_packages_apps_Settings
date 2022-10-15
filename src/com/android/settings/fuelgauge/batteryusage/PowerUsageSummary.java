acc1130799d3f735705548393de55f5cbbae6407
Pranav Vashi <neobuddy89@gmail.com>
2022-10-15 13:01:34 +0530

    Settings: Properly set battery temperature
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -129,6 +129,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
                     mBatteryHeaderPreferenceController.updateHeaderByBatteryTips(
                             mBatteryTipPreferenceController.getCurrentBatteryTip(), batteryInfo);
                     mBatteryInfo = batteryInfo;
+                    mBatteryTempPref.setSummary(mBatteryInfo.batteryTemp + " \u2103");
                 }
 
                 @Override
@@ -268,8 +269,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
         }
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
-
-        mBatteryTempPref.setSummary(BatteryInfo.batteryTemp / 10 + " °C");
 
         if (mBatteryHealthSupported) {
             mCurrentBatteryCapacity.setSummary(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity)));
