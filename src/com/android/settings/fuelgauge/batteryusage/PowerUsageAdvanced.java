5438873d0f57f9050429576c34c618de9eeb2403
ShevT <evgeny.shishkov@gmail.com>
2022-12-02 16:25:33 +0300

    Settings: Reset battery stats [2/3]
    Adapted for PowerUsageAdvanced
    
    Based on:
     From: Jorge Ruesga <jorge@ruesga.com>
     Date: Tue, 24 Nov 2015 23:37:56 +0800
     Subject: [PATCH 1/2] Settings: Reset battery stats [2/3]
    
     AOSiP edits: Use AOSP 'ok' string rather than add our own.
    
     Change-Id: I859c694d13085b48e363b0537c6c1d384acb8989
     Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Also included:
     From: Danny Lin <danny@kdrag0n.dev>
     Date: Sun, 13 Dec 2020 18:09:23 -0800
     Subject: [PATCH 2/2] Settings: Improve battery stats reset strings [1/2]
    
     In accordance with Material text guidelines.
    
     Change-Id: I106d2494e21646a4e96599bcbd0d40ecce4ea562
    
    @neobuddy89:
    Remove unnecessary battery stats permission.

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageAdvanced.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageAdvanced.java
@@ -18,7 +18,9 @@ package com.android.settings.fuelgauge.batteryusage;
 import static com.android.settings.fuelgauge.BatteryBroadcastReceiver.BatteryUpdateType;
 
 import android.app.settings.SettingsEnums;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
@@ -28,6 +30,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.SearchIndexableResource;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -55,6 +60,9 @@ public class PowerUsageAdvanced extends PowerUsageBase {
     private static final String KEY_REFRESH_TYPE = "refresh_type";
     private static final String KEY_BATTERY_GRAPH = "battery_graph";
     private static final String KEY_APP_LIST = "app_list";
+
+    static final int MENU_STATS_RESET = Menu.FIRST + 1;
+    private boolean mStatsReset = false;
 
     @VisibleForTesting
     BatteryHistoryPreference mHistPref;
@@ -139,6 +147,34 @@ public class PowerUsageAdvanced extends PowerUsageBase {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        MenuItem reset = menu.add(0, MENU_STATS_RESET, 0, R.string.battery_stats_reset)
+                .setIcon(R.drawable.ic_delete)
+                .setAlphabeticShortcut('d');
+        reset.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void resetStats() {
+        BatteryManager batteryManager = getContext().getSystemService(BatteryManager.class);
+        AlertDialog dialog = new AlertDialog.Builder(getActivity())
+            .setTitle(R.string.battery_stats_reset)
+            .setMessage(R.string.battery_stats_message)
+            .setPositiveButton(R.string.battery_stats_clear, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    batteryManager.resetStatistics();
+                    mStatsReset = true;
+                    refreshUi(BatteryUpdateType.MANUAL);
+                }
+            })
+            .setNegativeButton(R.string.cancel, null)
+            .create();
+        dialog.show();
+    }
+
+    @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
         refreshFeatureFlag(context);
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
@@ -164,6 +200,17 @@ public class PowerUsageAdvanced extends PowerUsageBase {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_STATS_RESET:
+                resetStats();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void refreshUi(@BatteryUpdateType int refreshType) {
         final Context context = getContext();
         if (context == null) {
@@ -177,6 +224,10 @@ public class PowerUsageAdvanced extends PowerUsageBase {
         }
         if (mBatteryChartPreferenceController != null && mBatteryHistoryMap != null) {
             mBatteryChartPreferenceController.setBatteryHistoryMap(mBatteryHistoryMap);
+        }
+        if (mStatsReset) {
+            restartBatteryStatsLoader(refreshType);
+            mStatsReset = false;
         }
     }
 
