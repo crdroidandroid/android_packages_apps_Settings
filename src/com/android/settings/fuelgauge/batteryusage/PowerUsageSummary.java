fd43ba62eedfaae145ef1d9ed5b56c9dd3b7743c
MLZ94 <zugutoroberto@gmail.com>
2023-01-29 09:57:53 +0100

    Revert "Settings: PowerUsageSummary: open advanced usage on header click"
    The ripple effect of the header is broken, and it creates redundancy as there is a preference to open advanced usage rught under the header
    
    This reverts commit bcd787e77195b3b4f4552ebb2e78409960b6a8ad.

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -37,7 +37,6 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
-import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.fuelgauge.BatteryHeaderPreferenceController;
 import com.android.settings.fuelgauge.BatteryInfo;
 import com.android.settings.fuelgauge.BatteryInfoLoader;
@@ -215,19 +214,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
         }
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (BatteryHeaderPreferenceController.KEY_BATTERY_HEADER.equals(preference.getKey())) {
-            new SubSettingLauncher(getContext())
-                        .setDestination(PowerUsageAdvanced.class.getName())
-                        .setSourceMetricsCategory(getMetricsCategory())
-                        .setTitleRes(R.string.advanced_battery_title)
-                        .launch();
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
