af58d73937136fa1c71648833d1fda3ea80dc34c
Pranav Vashi <neobuddy89@gmail.com>
2023-03-04 22:07:20 +0530

    Settings: Update AppLock API
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/security/applock/AppLockPackageProtectionPC.kt
+++ src/com/android/settings/security/applock/AppLockPackageProtectionPC.kt
@@ -43,9 +43,9 @@ class AppLockPackageProtectionPC(
     init {
         coroutineScope.launch {
             isProtected = withContext(Dispatchers.Default) {
-                appLockManager.packageData.any {
+                appLockManager.packageData.find {
                     it.packageName == packageName
-                }
+                }?.shouldProtectApp == true
             }
             preference?.let {
                 updateState(it)
@@ -61,11 +61,7 @@ class AppLockPackageProtectionPC(
         if (isProtected == checked) return false
         isProtected = checked
         coroutineScope.launch(Dispatchers.Default) {
-            if (isProtected) {
-                appLockManager.addPackage(packageName)
-            } else {
-                appLockManager.removePackage(packageName)
-            }
+            appLockManager.setShouldProtectApp(packageName, isProtected)
         }
         return true
     }
