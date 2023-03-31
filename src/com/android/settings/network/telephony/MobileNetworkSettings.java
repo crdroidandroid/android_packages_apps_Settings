37767ffec8e2a612516951dfdf65dc805935a45d
DarkJoker360 <simoespo159@gmail.com>
2023-03-31 10:42:42 +0000

    Settings: Add a toggle to force LTE_CA
    Change-Id: I017be698dc6c438edf2236df7c0125c05202bf3b
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -331,6 +331,7 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
         use(ContactDiscoveryPreferenceController.class).init(getParentFragmentManager(), mSubId);
         use(NrAdvancedCallingPreferenceController.class).init(mSubId);
         use(TransferEsimPreferenceController.class).init(mSubId, mSubscriptionInfoEntity);
+        use(ForceLteCaPreferenceController.class).init(mSubId);
         final ConvertToEsimPreferenceController convertToEsimPreferenceController =
                 use(ConvertToEsimPreferenceController.class);
         if (convertToEsimPreferenceController != null) {
