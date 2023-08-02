cc293d4adf3725b5e68ba6d7533267d813ac6fc0
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-02 16:25:46 +0200

    Settings: Fix findPeakRefreshRate logic
     * findPeakRefreshRate(display.getSupportedModes()) would return the panels max supported refresh rate and not the one set in config_defaultPeakRefreshRate resulting it to basically ignore that overlay
     * this way it uses the highest available refresh rate if config_defaultPeakRefreshRate is not defined, otherwise it will take the highest available value up to the specified value in that overlay
    
    Change-Id: If515ff4ca972e0582e88330474b251d83246918f
    (cherry picked from commit 4a0f1ce1952789a3235a201fcc0cb788ecbebf6a)
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/display/PeakRefreshRatePreferenceController.java
+++ src/com/android/settings/display/PeakRefreshRatePreferenceController.java
@@ -141,9 +141,13 @@ public class PeakRefreshRatePreferenceController extends TogglePreferenceControl
     @VisibleForTesting
     float findPeakRefreshRate(Display.Mode[] modes) {
         float peakRefreshRate = DEFAULT_REFRESH_RATE;
+        float defaultPeakRefreshRate = (float) mContext.getResources().getInteger(
+                    com.android.internal.R.integer.config_defaultPeakRefreshRate);
         for (Display.Mode mode : modes) {
-            if (Math.round(mode.getRefreshRate()) > peakRefreshRate) {
-                peakRefreshRate = mode.getRefreshRate();
+            float refreshRate = Math.round(mode.getRefreshRate());
+            if (refreshRate > peakRefreshRate
+                    && (defaultPeakRefreshRate == 0 || refreshRate <= defaultPeakRefreshRate)) {
+                peakRefreshRate = refreshRate;
             }
         }
         return peakRefreshRate;
