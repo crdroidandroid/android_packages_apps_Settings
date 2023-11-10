5d1622dc8cd1122dd1e9be79b1ddcbf1530980cb
Pranav Vashi <neobuddy89@gmail.com>
2023-11-11 00:53:28 +0530

    Settings: Remove unused BatteryUtils in PowerUsageSummary
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -39,7 +39,6 @@ import com.android.settings.Utils;
 import com.android.settings.fuelgauge.BatteryHeaderPreferenceController;
 import com.android.settings.fuelgauge.BatteryInfo;
 import com.android.settings.fuelgauge.BatteryInfoLoader;
-import com.android.settings.fuelgauge.BatteryUtils;
 import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.fuelgauge.batterytip.BatteryTipLoader;
 import com.android.settings.fuelgauge.batterytip.BatteryTipPreferenceController;
@@ -79,8 +78,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
     PowerGaugePreference mBatteryTempPref;
     @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
-    @VisibleForTesting
-    BatteryUtils mBatteryUtils;
     @VisibleForTesting
     BatteryInfo mBatteryInfo;
     @VisibleForTesting
@@ -188,7 +185,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
                 KEY_DESIGNED_BATTERY_CAPACITY);
         mBatteryChargeCycles = (PowerGaugePreference) findPreference(
                 KEY_BATTERY_CHARGE_CYCLES);
-        mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         mBatteryHealthSupported = getResources().getBoolean(R.bool.config_supportBatteryHealth);
         if (!mBatteryHealthSupported) {
