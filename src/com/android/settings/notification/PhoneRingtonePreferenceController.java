84f475dd78fec5c3e2bb6b8d7ecc204dc9ff24d2
ShevT <evgeny.shishkov@gmail.com>
2023-12-19 11:25:02 +0300

    Settings: Add simcard number title for SIM1

--- src/com/android/settings/notification/PhoneRingtonePreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtonePreferenceController.java
@@ -46,7 +46,8 @@ public class PhoneRingtonePreferenceController extends RingtonePreferenceControl
         if (telephonyManager.isMultiSimEnabled()) {
             DefaultRingtonePreference ringtonePreference =
                     (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE);
-            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title));
+            ringtonePreference.setTitle(mContext.getString(R.string.ringtone_title) + " - " +
+                String.format(mContext.getString(R.string.sim_card_number_title), 1));
             ringtonePreference.setEnabled(hasCard());
         }
     }
