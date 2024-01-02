b48d64331c97e9023897e02c954faeb043e80651
Pranav Vashi <neobuddy89@gmail.com>
2024-01-02 23:57:58 +0530

    Settings: Let app lock use face unlock
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/security/applock/AppLockCredentialActivity.kt
+++ src/com/android/settings/security/applock/AppLockCredentialActivity.kt
@@ -125,7 +125,7 @@ class AppLockCredentialActivity : FragmentActivity() {
         )
         var allowedAuthenticators = Authenticators.DEVICE_CREDENTIAL
         if (biometricsAllowed) {
-            allowedAuthenticators = allowedAuthenticators or Authenticators.BIOMETRIC_STRONG
+            allowedAuthenticators = allowedAuthenticators or Authenticators.BIOMETRIC_WEAK
         }
 
         val promptInfo = PromptInfo().apply {
