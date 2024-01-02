cef9658116f4f1bfaf28e1d1660d217c894e8c15
Pranav Vashi <neobuddy89@gmail.com>
2024-01-02 23:53:00 +0530

    Revert "Settings: Let clone apps preference be available with config"
    This reverts commit 52b92aeca7783cc24efe1965cad7e180d00bc01b.

--- src/com/android/settings/applications/ClonedAppsPreferenceController.java
+++ src/com/android/settings/applications/ClonedAppsPreferenceController.java
@@ -55,7 +55,9 @@ public class ClonedAppsPreferenceController extends BasePreferenceController
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_cloned_apps_page_enabled)
+        return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_APP_CLONING,
+                PROPERTY_CLONED_APPS_ENABLED, true)
+                && mContext.getResources().getBoolean(R.bool.config_cloned_apps_page_enabled)
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
