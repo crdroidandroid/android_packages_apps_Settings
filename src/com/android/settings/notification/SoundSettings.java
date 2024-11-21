dedd4a00ecfbc7b4156c765f713be75685a701e6
Pranav Vashi <neobuddy89@gmail.com>
2024-11-21 06:31:15 +0530

    Settings: Add config to display Now Playing
    * Disabled by default as most devices dont support it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -32,6 +32,7 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.RingtonePreference;
@@ -60,6 +61,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
 
     private static final String EXTRA_OPEN_PHONE_RINGTONE_PICKER =
             "EXTRA_OPEN_PHONE_RINGTONE_PICKER";
+    private static final String KEY_NOW_PLAYING = "dashboard_tile_pref_com.google.intelligence.sense.ambientmusic.AmbientMusicSettingsActivity";
 
     @VisibleForTesting
     static final int STOP_SAMPLE = 1;
@@ -113,6 +115,19 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         Preference phoneRingTonePreference = findPreference("phone_ringtone");
         if (phoneRingTonePreference != null && openPhoneRingtonePicker) {
             onPreferenceTreeClick(phoneRingTonePreference);
+        }
+        updateAmbientMusicPref();
+    }
+
+    private void updateAmbientMusicPref() {
+        final PreferenceScreen screen = getPreferenceScreen();
+        if (getContext().getResources().getBoolean(R.bool.config_show_now_playing) || screen == null) {
+            return;
+        }
+
+        final Preference preference = screen.findPreference(KEY_NOW_PLAYING);
+        if (preference != null) {
+            screen.removePreference(preference);
         }
     }
 
@@ -341,6 +356,17 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(R.xml.sound_settings) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    if (!context.getResources().getBoolean(R.bool.config_show_now_playing)) {
+                        keys.add(KEY_NOW_PLAYING);
+                    }
+
+                    return keys;
+                }
 
                 @Override
                 public List<AbstractPreferenceController> createPreferenceControllers(
