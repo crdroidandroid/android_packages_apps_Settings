1119e4e8302a610704b5b4e2cc9d82fc97de791e
Pranav Vashi <neobuddy89@gmail.com>
2024-11-21 06:31:15 +0530

    Settings: Add config to display Now Playing
    * Disabled by default as most devices dont support it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -30,6 +30,8 @@ import android.os.Looper;
 import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -70,7 +72,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
     @VisibleForTesting
     static final String KEY_ADD_USER_FROM_LOCK_SCREEN =
             "security_lockscreen_add_users_when_locked";
-
+    private static final String KEY_NOW_PLAYING = "dashboard_tile_pref_com.google.intelligence.sense.ambientmusic.AmbientMusicNotificationsSettingsActivity";
 
     private AmbientDisplayConfiguration mConfig;
     private OwnerInfoPreferenceController mOwnerInfoPreferenceController;
@@ -95,6 +97,18 @@ public class LockscreenDashboardFragment extends DashboardFragment
                 R.string.locked_work_profile_notification_title);
         replaceEnterpriseStringTitle("security_setting_lock_screen_notif_work_header",
                 WORK_PROFILE_NOTIFICATIONS_SECTION_HEADER, R.string.profile_section_header);
+        updateAmbientMusicPref();
+    }
+
+    private void updateAmbientMusicPref() {
+        final PreferenceScreen screen = getPreferenceScreen();
+        if (getContext().getResources().getBoolean(R.bool.config_show_now_playing) || screen == null) {
+            return;
+        }
+        final Preference preference = screen.findPreference(KEY_NOW_PLAYING);
+        if (preference != null) {
+            screen.removePreference(preference);
+        }
     }
 
     @Override
@@ -186,6 +200,9 @@ public class LockscreenDashboardFragment extends DashboardFragment
                 public List<String> getNonIndexableKeys(Context context) {
                     final List<String> niks = super.getNonIndexableKeys(context);
                     niks.add(KEY_ADD_USER_FROM_LOCK_SCREEN);
+                    if (!context.getResources().getBoolean(R.bool.config_show_now_playing)) {
+                        niks.add(KEY_NOW_PLAYING);
+                    }
                     return niks;
                 }
 
