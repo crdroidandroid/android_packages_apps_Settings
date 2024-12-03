fdbd12f89a40b83161b9a063e0d61c60469944be
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-03 08:37:09 +0800

    TopLevelSettings: Fix null pointer crash on high dpi devices
    * reference issue: https://github.com/RisingTechOSS/issue_tracker/issues/49
    
    cc: mnri
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -52,6 +52,7 @@ import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.support.SupportPreferenceController;
 import com.android.settings.widget.HomepagePreference;
+import com.android.settings.widget.HomepagePreferenceLayoutHelper;
 import com.android.settings.widget.HomepagePreferenceLayoutHelper.HomepagePreferenceLayout;
 import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.drawer.Tile;
@@ -285,10 +286,11 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             @Override
             public void doForEach(Preference preference) {
                 if (preference instanceof HomepagePreferenceLayout) {
-                    ((HomepagePreferenceLayout) preference).getHelper()
-                            .setIconPaddingStart(mIconPaddingStart);
-                    ((HomepagePreferenceLayout) preference).getHelper()
-                            .setTextPaddingStart(mTextPaddingStart);
+                    HomepagePreferenceLayoutHelper helper = ((HomepagePreferenceLayout) preference).getHelper();
+                    if (helper != null) {
+                        helper.setIconPaddingStart(mIconPaddingStart);
+                        helper.setTextPaddingStart(mTextPaddingStart);
+                    }
                 }
             }
         });
