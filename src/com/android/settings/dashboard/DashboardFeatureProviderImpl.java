a2acf4dace58a48430b01bbe1e0d6ea2795b361f
Pranav Vashi <neobuddy89@gmail.com>
2025-01-05 20:55:37 +0530

    Revert "Settings: Fix loading iconpack-based Wellbeing icon"
    This reverts commit 7a74dd410c4436acc21463b6328b22ad4841703b.

--- src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
@@ -42,7 +42,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
-import android.graphics.drawable.LayerDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -93,7 +92,6 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
     private static final String TAG = "DashboardFeatureImpl";
     private static final String DASHBOARD_TILE_PREF_KEY_PREFIX = "dashboard_tile_pref_";
     private static final String META_DATA_KEY_INTENT_ACTION = "com.android.settings.intent.action";
-    private static final String WELLBEING_PACKAGE = "com.google.android.apps.wellbeing";
 
     protected final Context mContext;
 
@@ -458,11 +456,6 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
         }
         // Tint homepage icons
         if (TextUtils.equals(tile.getCategory(), CategoryKey.CATEGORY_HOMEPAGE)) {
-            if (iconPackage.equals(WELLBEING_PACKAGE) && iconDrawable instanceof LayerDrawable
-                    && ((LayerDrawable) iconDrawable).getDrawable(1) != null) {
-                iconDrawable = ((LayerDrawable) iconDrawable).getDrawable(1);
-                iconDrawable.mutate();
-            }
             // Skip tinting and Adaptive Icon transformation for homepage account type raw icons
             if (TextUtils.equals(tile.getGroupKey(), "top_level_account_category")
                     && iconPackage == null) {
