ead1ee8e3e3b6896754ebcd1764fc7b0c55f54db
Pranav Vashi <neobuddy89@gmail.com>
2025-03-26 21:53:46 +0530

    Settings: Do not hide navigation modes
    * We have customization for 3 buttons included here.
    * Instead hide "Gesture Navigation" if config_swipe_up_gesture_setting_available is disabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -215,7 +215,8 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         List<CandidateInfoExtra> candidates = new ArrayList<>();
 
         if (SystemNavigationPreferenceController.isOverlayPackageAvailable(c,
-                NAV_BAR_MODE_GESTURAL_OVERLAY)) {
+                NAV_BAR_MODE_GESTURAL_OVERLAY)
+                && SystemNavigationPreferenceController.isGestureAvailable(c)) {
             candidates.add(new CandidateInfoExtra(
                     c.getText(R.string.edge_to_edge_navigation_title),
                     c.getText(R.string.edge_to_edge_navigation_summary),
@@ -394,7 +395,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
 
                 @Override
                 protected boolean isPageSearchEnabled(Context context) {
-                    return SystemNavigationPreferenceController.isGestureAvailable(context);
+                    return true;
                 }
 
                 @Override
@@ -404,7 +405,8 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
                     final List<SearchIndexableRaw> result = new ArrayList<>();
 
                     if (SystemNavigationPreferenceController.isOverlayPackageAvailable(context,
-                            NAV_BAR_MODE_GESTURAL_OVERLAY)) {
+                            NAV_BAR_MODE_GESTURAL_OVERLAY) &&
+                            SystemNavigationPreferenceController.isGestureAvailable(context)) {
                         SearchIndexableRaw data = new SearchIndexableRaw(context);
                         data.title = res.getString(R.string.edge_to_edge_navigation_title);
                         data.key = KEY_SYSTEM_NAV_GESTURAL;
