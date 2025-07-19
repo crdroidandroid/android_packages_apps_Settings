02fab29342b06a17d16234c1ef038a417caaa7af
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-19 19:02:21 +0800

    Settings: add three finger swipe actions/screenshot combo intercept
    Change-Id: I605bdf2e5f975edef7a4865ded35345a085799a7

---
+++ src/com/android/settings/gestures/ThreeFingerSwipePreferenceController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class ThreeFingerSwipePreferenceController extends BasePreferenceController {
+
+    public ThreeFingerSwipePreferenceController(@NonNull Context context, @NonNull String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public void displayPreference(@NonNull PreferenceScreen screen) {
+        super.displayPreference(screen);
+    }
+
+    @Override
+    @NonNull
+    public CharSequence getSummary() {
+        return mContext.getText(R.string.nt_gesture_three_finger_swipe_summary);
+    }
+}
