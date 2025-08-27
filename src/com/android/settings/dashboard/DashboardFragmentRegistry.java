4ae0bd7c146f5a9d1cadaaff83828adb06ef7ba6
Pranav Vashi <neobuddy89@gmail.com>
2025-08-27 21:46:44 +0530

    Settings: Remove unused category
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -51,8 +51,6 @@ import com.android.settings.supervision.SupervisionDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
-import com.crdroid.settings.crDroidSettingsLayout;
-
 import java.util.Map;
 
 /**
@@ -137,8 +135,6 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SupervisionDashboardFragment.class.getName(),
                 CategoryKey.CATEGORY_SUPERVISION);
-        PARENT_TO_CATEGORY_KEY_MAP.put(crDroidSettingsLayout.class.getName(),
-                CategoryKey.CATEGORY_CRDROID);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
