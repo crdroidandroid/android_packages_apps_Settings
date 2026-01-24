b72643b6b0ea99d083ebbf1c7f57939dceaf2723
Pranav Vashi <neobuddy89@gmail.com>
2026-01-24 23:21:12 +0530

    Settings: Remove flag check for blur toggle
    * How many guards are we going to add for blur? :/
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/display/EnableBlursPreferenceController.java
+++ src/com/android/settings/display/EnableBlursPreferenceController.java
@@ -62,7 +62,7 @@ public final class EnableBlursPreferenceController extends AbstractPreferenceCon
 
     @Override
     public boolean isAvailable() {
-        return !com.android.systemui.Flags.blurSettingsToggle() && mBlurSupported;
+        return mBlurSupported;
     }
 
     @Override
