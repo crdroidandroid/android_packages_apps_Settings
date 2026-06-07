758726613610ab6728fc000f9cedbb87c52a8cd1
Pranav Vashi <neobuddy89@gmail.com>
2026-06-07 19:40:09 +0530

    Settings: Rewrite kernel version pref to replicate old behavior
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreference.kt
@@ -1,31 +1,28 @@
 /*
- * Copyright (C) 2024 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.settings.deviceinfo.firmwareversion
 
 import android.content.Context
+import android.util.Log
 import androidx.preference.Preference
 import com.android.settings.R
 import com.android.settingslib.DeviceInfoUtils
 import com.android.settingslib.metadata.PreferenceMetadata
 import com.android.settingslib.metadata.PreferenceSummaryProvider
 import com.android.settingslib.preference.PreferenceBinding
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
 
 // LINT.IfChange
-class KernelVersionPreference : PreferenceMetadata, PreferenceSummaryProvider, PreferenceBinding {
+class KernelVersionPreference :
+    PreferenceMetadata, PreferenceSummaryProvider, PreferenceBinding,
+    Preference.OnPreferenceClickListener {
+
+    private var fullKernelVersion = false
 
     override val key: String
         get() = "kernel_version"
@@ -38,8 +35,37 @@ class KernelVersionPreference : PreferenceMetadata, PreferenceSummaryProvider, P
 
     override fun bind(preference: Preference, metadata: PreferenceMetadata) {
         super.bind(preference, metadata)
-        preference.isSelectable = false
+        preference.isSelectable = true
         preference.isCopyingEnabled = true
+        preference.onPreferenceClickListener = this
+    }
+
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        if (fullKernelVersion) {
+            preference.summary = DeviceInfoUtils.getFormattedKernelVersion(preference.context)
+            fullKernelVersion = false
+        } else {
+            preference.summary = getFullKernelVersion()
+            fullKernelVersion = true
+        }
+        return true
+    }
+
+    private fun getFullKernelVersion(): CharSequence =
+        try {
+            readLine(FILENAME_PROC_VERSION) ?: "Unavailable"
+        } catch (e: IOException) {
+            Log.e(LOG_TAG, "IO Exception when getting kernel version for Device Info screen", e)
+            "Unavailable"
+        }
+
+    companion object {
+        private const val LOG_TAG = "KernelVersionPreference"
+        private const val FILENAME_PROC_VERSION = "/proc/version"
+
+        @Throws(IOException::class)
+        private fun readLine(filename: String): String? =
+            BufferedReader(FileReader(filename), 256).use { it.readLine() }
     }
 }
 // LINT.ThenChange(KernelVersionPreferenceController.java)
