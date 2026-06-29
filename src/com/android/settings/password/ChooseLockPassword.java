86ae99bdbbd3bb18b0ed3beac967462e93da10a2
Pranav Vashi <neobuddy89@gmail.com>
2026-06-29 11:36:09 +0530

    Settings: Fix color state for auto-confirm PIN checkbox
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/1048
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/password/ChooseLockPassword.java
+++ src/com/android/settings/password/ChooseLockPassword.java
@@ -626,7 +626,6 @@ public class ChooseLockPassword extends SettingsActivity {
             setOnAutoConfirmOptionClickListener();
             if (mAutoPinConfirmOption != null) {
                 mAutoPinConfirmOption.setAccessibilityLiveRegion(ACCESSIBILITY_LIVE_REGION_POLITE);
-                mAutoPinConfirmOption.setVisibility(View.GONE);
                 mAutoPinConfirmOption.setChecked(false);
             }
 
